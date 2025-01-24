dbd5be96a24edeb7c76f27330cd781436c0546f3
Liam Cotter <122333311@umail.ucc.ie>
2025-01-24 21:21:48 +0000

    Added docstrings to the GTFSR class.

--- back-end/gtfsr.py
+++ back-end/gtfsr.py
@@ -6,27 +6,49 @@ load_dotenv()
 
 
 class GTFSR:
+    """A class to make requests to the GTFSR API. All methods can be used without initialising the class."""
     base_url = "https://api.nationaltransport.ie/gtfsr/v2/"
     format_json = "?format=json"
     api_key = os.getenv("API_KEY", None)
 
-    def __init__(self) -> None:
-        pass
-
     @classmethod
     def fetch_vehicles(self) -> dict:
+        """
+        Fetches the vehicles endpoint from the GTFSR API.
+
+        Returns:
+            dict: The response from the GTFSR API
+        """
         return self.fetch_endpoint("Vehicles")
 
     @classmethod
     def fetch_trip_updates(self) -> dict:
+        """
+        Fetches the trip_updates endpoint from the GTFSR API.
+
+        Returns:
+            dict: The response from the GTFSR API
+        """
         return self.fetch_endpoint("TripUpdates")
 
     @classmethod
     def fetch_gtfsr(self) -> dict:
+        """
+        Fetches the gtfsr endpoint from the GTFSR API.
+
+        Returns:
+            dict: The response from the GTFSR API
+        """
         return self.fetch_endpoint("gtfsr")
 
     @classmethod
     def fetch_endpoint(self, endpoint: str) -> dict:
+        """
+        Makes a request to the GTFSR API at the specified endpoint.
+
+        Returns:
+            dict: A JSON formatted response from the GTFSR API
+        """
         url = self.base_url + endpoint + self.format_json
         headers = {
             'Cache-Control': 'no-cache',
