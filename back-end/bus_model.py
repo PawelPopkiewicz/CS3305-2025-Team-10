70398005af615e36159f62ce5cd331e936775058
Liam Cotter <122333311@umail.ucc.ie>
2025-01-26 13:10:24 +0000

    Added Agency class and updated Routes class

--- back-end/bus_model.py
+++ back-end/bus_model.py
@@ -1,22 +1,36 @@
 class Bus:
     """A class to represent a bus and its relevant information."""
-    all_buses = []
+    all_buses = {}
 
-    def __init__(self):
-        self.all_buses.append(self)
+    def __init__(self, bus_id):
+        self.all_buses[bus_id] = self
 
 
 class Stop:
     """A class to represent a bus stop and its relevant information."""
-    all_stops = []
+    all_stops = {}
 
-    def __init__(self):
-        self.all_stops.append(self)
+    def __init__(self, stop_id):
+        self.all_stops[stop_id] = self
 
 
 class Route:
     """A class to represent a bus route and its relevant information."""
-    all_routes = []
+    all_routes = {}
 
-    def __init__(self):
-        self.all_routes.append(self)
+    def __init__(self, route_id: int, agency_id: int, route_short_name: str, route_long_name: str, route_type: int):
+        self.all_routes[route_id] = self
+        self.route_id = route_id
+        self.agency_id = agency_id
+        self.route_short_name = route_short_name
+        self.route_long_name = route_long_name
+        self.route_type = route_type
+
+class Agency:
+    """A class to represent a bus agency and its relevant information."""
+    all_agencies = {}
+
+    def __init__(self, agency_id: int, agency_name: str):
+        self.all_agencies[agency_id] = self
+        self.agency_id = agency_id
+        self.agency_name = agency_name
