d93fc01f6d17bf9cf42a0a1d2c38fc2b033bf304
pawel <pawel.m.popkiewicz@gmail.com>
2025-01-28 19:30:38 +0000

    Create different map pages based on platform, make a component for shared features
    Signed-off-by: pawel <pawel.m.popkiewicz@gmail.com>

--- front-end/app/screens/map.tsx
+++ front-end/app/screens/map.tsx
@@ -1,36 +1,7 @@
-import {FlatList, StyleSheet, Text, TextInput, View} from "react-native";
-import ButtonTab from "../../components/ButtonTab";
-import {SetStateAction, useState} from "react";
-
+import { Platform } from 'react-native';
+import MobileMap from '@/app/screens/mobileMap';
+import WebMap from '@/app/screens/webMap';
 
 export default function Map() {
-    const data = ['220', 'WGB stop', 'Cherry', 'Date', 'Elderberry', 'Fig', 'Grape'];
-    const [searchQuery, setSearchQuery] = useState('');
-    const [filteredData, setFilteredData] = useState(data);
-
-    const handleSearch = (query: SetStateAction<string>) => {
-        setSearchQuery(query);
-        if (query) {
-            const filtered = data.filter(item => item.toLowerCase().includes(searchQuery.toLowerCase()));
-            setFilteredData(filtered);
-        } else {
-            setFilteredData(data);
-        }
-    };
-
-    return (
-        <View style={styles.container}>
-            <TextInput defaultValue={searchQuery} onChangeText={handleSearch} />
-            <FlatList
-                data={filteredData}
-                keyExtractor={(item) => item}
-            renderItem={({ item }) => (<Text>{item}</Text>)}/>
-            <ButtonTab />
-        </View>
-    )
+    return Platform.OS === 'web' ? <WebMap /> : <MobileMap />;
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
-})
