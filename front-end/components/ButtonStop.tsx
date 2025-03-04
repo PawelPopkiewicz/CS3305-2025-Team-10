d368c5f8de155d5762edd67c21f5b51b65700a35
pawel <pawel.m.popkiewicz@gmail.com>
2025-03-04 20:39:42 +0000

    add filtering of stops by fav
    Signed-off-by: pawel <pawel.m.popkiewicz@gmail.com>

--- front-end/components/ButtonStop.tsx
+++ front-end/components/ButtonStop.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {router} from 'expo-router';
+
 import colors from '@/config/Colors';
 import fonts from '@/config/Fonts';
 import {Stop} from "@/types/stop";
@@ -8,21 +9,20 @@ import {shallowEqual, useSelector} from "react-redux";
 import {RootState} from "@/app/redux/store";
 
 const ButtonStop = () => {
-
-
     const stops = useSelector((state: RootState) => state.stop.stops, shallowEqual);
-
+    const favs = useSelector((state: RootState) => state.fav.stops, shallowEqual);
+    const favStops = stops.filter((stop: Stop) => favs.includes(stop.id));
     return (
         <View>
 
             {/* create component for each stop */}
-            {stops.map((stop: Stop) => (
+            {favStops.map((stop: Stop) => (
 
                 <View key={stop.id} style={styles.buttonContainer}>
 
                     <TouchableOpacity
                     style={styles.buttonContainer}
-                    onPress={() => router.push({ pathname: `/screens/arrivals/${stop.id}`, params: { stop: stop.id } })}      // forward data of the selected stop to stop page
+                    onPress={() => router.push({ pathname: `/screens/arrivals/${String(stop.id)}`, params: { stop: stop.id } })}      // forward data of the selected stop to stop page
                     activeOpacity={0.1}
                     >
                         <View>
