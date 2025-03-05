12318ac8d9b0bc737d886ade4197fcb444179ffc
Aleksandr Berdin <kvit.beats2@gmail.com>
2025-03-05 15:23:01 +0000

    Introduce loading component

--- front-end/components/Loading.tsx
+++ front-end/components/Loading.tsx
@@ -4,13 +4,14 @@ import {router} from 'expo-router';
 
 import colors from '@/config/Colors';
 import fonts from '@/config/Fonts';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Loading = ({ text }) => {
     return (
-        <View>
+        <SafeAreaView style={styles.loading}>
                 <Text>{text}</Text>
                 <ActivityIndicator size="large" color="#00ff00" />
-        </View>
+        </SafeAreaView>
     );
 };
 
@@ -19,6 +20,10 @@ const styles = StyleSheet.create({
         backgroundColor: colors.backgroundPrimary,
         padding: 5,
         paddingHorizontal: 7,
+    },
+    loading: {
+        backgroundColor: colors.backgroundPrimary,
+        textAlign: "center",
     },
     
 });
