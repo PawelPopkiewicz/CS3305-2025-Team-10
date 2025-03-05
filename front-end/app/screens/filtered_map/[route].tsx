6af046a72ccd61d5e97589488c005462640a4df9
pawel <pawel.m.popkiewicz@gmail.com>
2025-03-05 01:31:09 +0000

    add a map with a route filter
    Signed-off-by: pawel <pawel.m.popkiewicz@gmail.com>

---
+++ front-end/app/screens/filtered_map/[route].tsx
@@ -0,0 +1,116 @@
+import React, {useCallback, useState} from "react";
+import {Platform, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity} from "react-native";
+import {Icon} from '@rneui/themed';
+import MapView, {Region} from "react-native-maps";
+import {router, useLocalSearchParams} from "expo-router";
+import {shallowEqual, useSelector} from "react-redux";
+// import ClusteredMapView from "react-native-maps-super-cluster";
+import BusMarker from "@/components/BusMarker";
+import StopMarker from "@/components/StopMarker";
+import colors from "@/config/Colors";
+import fonts from "@/config/Fonts";
+import {RootState} from "@/app/redux/store";
+import {DEFAULT_REGION} from "@/config/constants";
+
+const FilteredMap = () => {
+    const { route } = useLocalSearchParams() as { route: string };
+    const stops = useSelector((state: RootState) => state.stop.stops, shallowEqual);
+    const buses = useSelector((state: RootState) => state.bus.buses, shallowEqual);
+
+    const [region, setRegion] = useState<Region>(DEFAULT_REGION);
+
+    // Filter only visible markers
+    const getVisibleMarkers = useCallback(() => {
+        return {
+            visibleStops: stops.filter(stop =>
+                stop.lat > region.latitude - region.latitudeDelta / 2 &&
+                stop.lat < region.latitude + region.latitudeDelta / 2 &&
+                stop.lon > region.longitude - region.longitudeDelta / 2 &&
+                stop.lon < region.longitude + region.longitudeDelta / 2
+            ),
+            visibleBuses: buses.filter(bus =>
+                bus.lat > region.latitude - region.latitudeDelta / 2 &&
+                bus.lat < region.latitude + region.latitudeDelta / 2 &&
+                bus.lon > region.longitude - region.longitudeDelta / 2 &&
+                bus.lon < region.longitude + region.longitudeDelta / 2 &&
+                bus.route === route
+            )
+        };
+    }, [stops, buses, region]);
+
+    const { visibleStops, visibleBuses } = getVisibleMarkers();
+
+    return (
+        <SafeAreaView style={styles.background}>
+            <TouchableOpacity style={styles.input} onPress={() => router.push("/screens/search")}>
+                <Icon iconStyle={styles.back} onPress={() => router.back()} name="chevron-left" type="font-awesome" />
+                <Text style={styles.textSecondary}>Search bus stop or route</Text>
+            </TouchableOpacity>
+
+            <MapView
+                style={{ flex: 1 }}
+                initialRegion={region}
+                rotateEnabled={false}
+                onRegionChangeComplete={setRegion} // Updates visible markers
+                // clusteringEnabled
+                // clusterColor={colors.objectSelected}
+                // clusterTextColor="#fff"
+            >
+                {/* Display Stop Markers */}
+                {/* Stop Markers */}
+                {visibleStops.map((stop) => (
+                    <StopMarker
+                        key={stop.id}
+                        id={stop.id}
+                        lat={stop.lat}
+                        lon={stop.lon}
+                        name={stop.name}
+                        code={stop.code}
+                    />
+                ))}
+
+                {/* Bus Markers */}
+                {visibleBuses.map((bus) => (
+                    <BusMarker key={bus.id} lat={bus.lat} lon={bus.lon} id={bus.id} route={bus.route} direction={bus.direction} />
+                ))}
+            </MapView>
+        </SafeAreaView>
+    );
+};
+
+// Memoized Marker Components
+// const MemoizedStopMarker = React.memo(({ lat, lon }: { lat: number, lon: number }) => (
+//     <StopMarker lat={lat} lon={lon} />
+// ));
+
+// const MemoizedBusMarker = React.memo(({ lat, lon }: { lat: number, lon: number }) => (
+//     <BusMarker lat={lat} lon={lon} />
+// ));
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: colors.backgroundPrimary,
+        overflow: 'hidden',
+    },
+    input: {
+        marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        overflow: 'hidden',
+        borderRadius: 30,
+        borderWidth: 1,
+        flexDirection: 'row',
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        borderColor: colors.border,
+    },
+    back: {
+        color: colors.textPrimary,
+        paddingRight: 10,
+    },
+    textSecondary: {
+        color: colors.textSecondary,
+        fontSize: fonts.subHeading,
+    }
+});
+
+export default Map;
